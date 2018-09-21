08bc43cb7c6e343b4d1d5c0e411fff020fb9f434
Hays Clark <hays.clark@gmail.com>
2018-09-21 16:39:44 -0700

    bug: fixing linting issue with Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,22 @@
-/*jshint camelcase: false */
+/* jshint camelcase: false */
 
 'use strict';
 
 module.exports = function (grunt) {
+  // Load multiple grunt tasks using globbing patterns
+  // https://github.com/sindresorhus/load-grunt-tasks
+  require('load-grunt-tasks')(grunt);
 
-	// Load multiple grunt tasks using globbing patterns
-	// https://github.com/sindresorhus/load-grunt-tasks
-	require('load-grunt-tasks')(grunt);
+  // load modular grunt configuration
+  // https://github.com/firstandthird/load-grunt-config
+  require('load-grunt-config')(grunt);
 
-	// load modular grunt configuration
-	// https://github.com/firstandthird/load-grunt-config
-	require('load-grunt-config')(grunt);
+  // Default task(s).
+  grunt.registerTask('default', ['mochacli']);
 
-	// Default task(s).
-	grunt.registerTask('default', ['jshint', 'mochacli']);
+  // tests task(s).
+  grunt.registerTask('test', ['mochacli']);
 
-	// tests task(s).
-	grunt.registerTask('test', ['mochacli']);
-
-	// code coverage task(s).
-	grunt.registerTask('coverage', ['mocha_istanbul']);
+  // code coverage task(s).
+  grunt.registerTask('coverage', ['mocha_istanbul']);
 };
